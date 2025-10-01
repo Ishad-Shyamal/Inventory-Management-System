bca6500b22ef075d28afdcb959c0e3cf6db031ef
Ishad-Shyamal <ishadss2016@gmail.com>
2025-10-01 23:02:32 +0530

    feat: add product model with schema definition

---
+++ server/models/product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    quantity: { type: number, required: true },
+    price: { type: number, reqired: true },
+    description: { type: String, required: true }
+});
+
+const Product = mongoose.model('product', productSchema);
+
+module.exports = Product;
